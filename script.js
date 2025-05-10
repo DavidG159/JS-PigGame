92639023b4e24039a9b296886af8ddd32def7bab
DavidG159 <gaspardavid235@gmail.com>
2025-05-10 15:05:02 -0600

    Added more functionality

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -18,6 +20,7 @@ diceEl.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let finalscore = 0;
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -33,13 +36,16 @@ btnRoll.addEventListener('click', function () {
         //Add dice to the current score
         currentScore += dice;
         document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-
+        finalscore += document.getElementById((`score${activePlayer}El`));
+        console.log(typeof finalscore);
 
     } else {
         //switch to the next player
         document.getElementById(`current--${activePlayer}`).textContent = 0;
         activePlayer = activePlayer === 0 ? 1 : 0;
         currentScore = 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
     }
 
 
